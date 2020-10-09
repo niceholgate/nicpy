7618123ef1e623412020d9d7dd4a5f1f093deb9b
microfitness <niceholgate@gmail.com>
2020-10-10 00:07:26 +0100

    Updated CacheDict to be able to persist data at a specified location. If the data is older than the saved time, the persisted data is replaced. This can, for example, avoid the need to regularly re-scrape web data which rarely changes, and is a handy way to persist any large data for debugging or other purposes.

--- CacheDict.py
+++ CacheDict.py
@@ -1,16 +1,34 @@
 from datetime import datetime
 from inspect import signature
+from pathlib import Path
+import pickle
+from definitions import DATA_PERSIST_DIRECTORY
+
 class CacheDict:
 
-    def __init__(self, func, initial_keys=[]):
+    def __init__(self, func, initial_keys=[], persist_filename = '', persist_lifetime_hours=None):
 
         self.func = func
         self.func_name = str(func).split(' ')[1]
         self.n_func_args = len(signature(func).parameters)
         self.cache_dict = {}
+
+        # If the persist_filename exists, load it
+        self.persist_filepath = Path(DATA_PERSIST_DIRECTORY)/persist_filename
+        if Path(self.persist_filepath).exists():
+            persist_load = pickle.load(open(self.persist_filepath, 'rb'))
+            persist_CacheDict = persist_load['CacheDict']
+            save_datetime = persist_load['save_datetime']
+            # If the save_datetime is more than persist_lifetime_hours ago, and the func details match, load the old cache_dict
+            if (datetime.now()-save_datetime).total_seconds()/60/60 < persist_lifetime_hours:
+                if persist_CacheDict.func == self.func and persist_CacheDict.n_func_args == self.n_func_args and persist_CacheDict.func_name == self.func_name:
+                    self.cache_dict = persist_CacheDict.cache_dict
+
         for key in initial_keys:
             self.check_key(key)
-            self.cache_dict[key] = self.func(*key)
+            key_str = tuple(str(el) for el in key)
+            self.cache_dict[key_str] = self.func(*key)
+
 
     def check_key(self, key):
         er = 'All keys must be tuples of length equal to number of parameters in the' \
@@ -20,11 +38,15 @@ class CacheDict:
 
     def get_key_value(self, key):
         self.check_key(key)
-        if key in self.cache_dict.keys():
-            return self.cache_dict[key]
+        key_str = tuple(str(el) for el in key)
+        if key_str in self.cache_dict.keys():
+            return self.cache_dict[key_str]
         else:
-            self.cache_dict[key] = self.func(*key)
-            return self.cache_dict[key]
+            self.cache_dict[key_str] = self.func(*key)
+            # Update the persisted CacheDict if an update occurs
+            if self.persist_filepath:
+                pickle.dump({'CacheDict':self, 'save_datetime':datetime.now()}, open(self.persist_filepath, 'wb'))
+            return self.cache_dict[key_str]
 
     def force_key_value(self, key, value):
         self.check_key(key)
@@ -47,6 +69,7 @@ def long_func(n):
         n -= 1
     return result
 
+# Example showing speedup due to caching
 if __name__=='__main__':
     long_func_cache = CacheDict(long_func)
     t1=datetime.now()
