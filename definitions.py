3ef2c30f3c1ecd5069b6b4429e1efb3985360110
Nicholas Holgate <niceholgate@gmail.com>
2025-12-14 13:25:28 +0800

    moref iles

---
+++ definitions.py
@@ -0,0 +1,8 @@
+POPPLER_BIN_PATH            = r'C:\dev\python_utils\poppler-0.68.0_x86\poppler-0.68.0\bin'
+TESSERACT_EXE_FILEPATH      = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+FFMPEG_FILEPATH             = r'C:\dev\python_utils\ffmpeg\bin\ffmpeg.exe'
+OPENWEATHER_API_KEY         = 'ea012e6e870ba99606487430667ccef9'
+WOLFRAM_APP_ID              = 'X64GU5-W6ALLJHWTE'
+DATA_PERSIST_DIRECTORY      = r'C:\dev\nicpy\persisted_data'
+TEST_DATA_DIR               = r'C:\dev\nicpy\test\test_data'
+ETF_PORTFOLIO_FILEPATH      = r'D:\Nick\Misc\Finances\ETF performance and portfolio\ETF performance and portfolio.xlsx'
